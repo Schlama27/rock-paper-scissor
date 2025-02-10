b9631288671639755e0dd720ef0a39a3519ffed7
Kevin Thai <kschlama27@gmail.com>
2025-02-10 00:32:33 -0800

    v2 of rock paper scissors

--- rps-js-file.js
+++ rps-js-file.js
@@ -1,4 +1,52 @@
 //Computer generates a random number from 1-3 and each corresponding number returns rock, paper, or scissor
+const buttonChoice = document.querySelectorAll('.btn-choice');
+const score = document.querySelector('#score');
+const computerChoiceDisplay = document.querySelector('#computer-choice');
+
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
+
+score.innerHTML = 'Score: 0 to 0';
+
+buttonChoice.forEach(function(e) {
+    e.addEventListener('click', function() {
+        let computerChoice = getComputerChoice();
+        let humanChoice = this.id;
+        let roundWinner = playRound(humanChoice, computerChoice);
+        if (roundWinner === 'computerWin') {
+            computerScore++;
+            alert(`You lose! Computer chose ${computerChoice}. The score is ${humanScore} to ${computerScore}.`)
+        } else if (roundWinner === 'humanWin') {
+            humanScore++;
+            alert(`You win! Computer chose ${computerChoice}. The score is ${humanScore} to ${computerScore}.`)
+        } else if (humanChoice === computerChoice) {
+            alert(`Tie. The score is ${humanScore} to ${computerScore}.`);
+        };
+        computerChoiceDisplay.innerHTML = `Computer Chose: ${capitalizeFirstLetter(computerChoice)}`;
+        score.innerHTML = `Score: ${humanScore} to ${computerScore}`
+        if (humanScore === 5) {
+            alert(`You have won the game. Final score is ${humanScore} to ${computerScore}. Congratulations!`);
+            score.innerHTML = 'YOU WIN!';
+            resetGame();
+        } else if (computerScore === 5) {
+            alert(`You have lost the game. Final score is ${humanScore} to ${computerScore}.`);
+            score.innerHTML = 'YOU LOSE!';
+            resetGame();
+        };
+    })
+});
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+}
+
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     
@@ -11,21 +59,8 @@ function getComputerChoice() {
     }
 }
 
-//Prompts for user to input rock, paper, or scissor
-function getHumanChoice() {
-    let userInput = prompt('Choose: Rock, Paper, or Scissor.').toLowerCase();
 
-    if (userInput === 'rock' ||   
-        userInput === 'paper' || 
-        userInput === 'scissor') {
-        return userInput;
-    } else {
-        alert('Invalid entry');
-    }
-}
 
-let humanScore = 0;
-let computerScore = 0;
 
 //Plays a round to determine winner
 function playRound(humanChoice, computerChoice) {
@@ -40,35 +75,3 @@ function playRound(humanChoice, computerChoice) {
             return 'humanWin';
         }
 }
-
-//Plays 5 rounds to determine winner of entire game depending who wins the majority of rounds
-function playGame() {
-    for (let round = 1; round <= 5; round++) {
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        let roundWinner = playRound(humanChoice, computerChoice);
-
-        if (roundWinner === 'computerWin') {
-            computerScore++;
-            alert(`You lose! Computer chose ${computerChoice}. The score is ${humanScore} to ${computerScore}.`)
-        } else if (roundWinner === 'humanWin') {
-            humanScore++;
-            alert(`You win! Computer chose ${computerChoice}. The score is ${humanScore} to ${computerScore}.`)
-        } else if (humanChoice === computerChoice) {
-            alert(`Tie. The score is ${humanScore} to ${computerScore}.`);
-        } else {
-            alert('Press ESC to restart game.');
-        }
-
-        if (humanScore > computerScore && round === 5) {
-            alert(`You have won the game. Final score is ${humanScore} to ${computerScore}. Congratulations!`);
-        } else if (humanScore < computerScore && round === 5) {
-            alert(`You have lost the game. Final score is ${humanScore} to ${computerScore}.`);
-        } else if (humanScore === computerScore && round === 5) {
-            alert(`The game ended in a Tie. Final score is ${humanScore} to ${computerScore}.`)
-        }
-    }
-}
-
-playGame();
-
